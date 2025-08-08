a93c8ad9fa092bdfbb04ff470440adf0db2934af
Yogesh Saini <yogesh720.ys@gmail.com>
2025-08-08 20:30:43 +0530

    "Database Models created"

---
+++ models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'UserName is required.'],
+        trim: true,
+        minlength: 2,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: [true, 'UserEmail is required.'],
+        trim: true,
+        unique: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'Please enter a valid email address.'],
+    },
+    password: {
+         type: String,
+        required: [true, 'UserPassword is required.'],
+        minlength: 6,
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', UserSchema);
+export default User;
